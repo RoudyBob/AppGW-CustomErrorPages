65493278a29f15eb75fa4e4d858663a03e6d97c9
Bob Roudebush <bobroud@microsoft.com>
2018-07-25 16:51:16 -0400

    Working version of templates w/sample custom pages

--- deployresources.ps1
+++ deployresources.ps1
@@ -1,6 +1,6 @@
-Connect-AzureRmAccount
+Login-AzureRmAccount
 
 Select-AzureRmSubscription -SubscriptionName "ACN CIO Cloud Lab"
 
-New-AzureRmResourceGroup -Name "bobroud-appgw1" -Location "East US"
-New-AzureRmResourceGroupDeployment -Mode Incremental -Name AppGWDeployment -ResourceGroupName bobroud-AppGW1 -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
+New-AzureRmResourceGroup -Name "bobroud-appgw3" -Location "East US"
+New-AzureRmResourceGroupDeployment -Mode Incremental -Name AppGWDeployment -ResourceGroupName bobroud-appgw3 -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
